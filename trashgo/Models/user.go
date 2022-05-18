f233b426a9501ae90627c71445bdaa94e5f9498e
bagja002 <81409623+bagja002@users.noreply.github.com>
2022-05-18 13:54:30 +0700

    Update user.go

--- trashgo/Models/user.go
+++ trashgo/Models/user.go
@@ -9,3 +9,13 @@ type User struct {
 	TempatLahir string `json:"tempat_lahir"`
 	Kelamin     string `json:"kelamin"`
 }
+
+type Aku struct {
+	jwt.StandardClaims
+	IDUser      int    `json:"Id_user"`
+	Email       string `gorm:"unique"`
+	NamaUser    string `json:"nama_user"`
+	Alamat      string `json:"alamat"`
+	TempatLahir string `json:"tempat_lahir"`
+	Kelamin     string `json:"kelamin"`
+}
